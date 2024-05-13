b7d50e223c385e7b7c6a8570ad35f47aaecfbdac
Ezekiel Fallarcuna <acefallarcuna.s@gmail.com>
2024-05-13 16:51:44 +0800

    final fix

---
+++ redirect.js
@@ -0,0 +1,4 @@
+document.getElementById("submitButton").addEventListener("click", function(event) {
+    event.preventDefault(); // prevent the default form submission
+    window.location.href = "logged.html";
+});
